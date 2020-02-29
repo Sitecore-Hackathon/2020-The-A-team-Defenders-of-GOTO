4e18ed6d28beb87caa08ce9811cac4478afd7773
Roland Villemoes <rv@alpha-solutions.us>
2020-02-29 12:20:23 -0500

    adding missing file

---
+++ src/Foundation/XConnect/GOTO-Usergroup.Foundation.XConnect/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GOTO_Usergroup.Foundation.XConnect
+{
+    public static class Constants 
+    {
+        public static string XConnectSourceName = "GOTO-UserGroup";
+    }
+}
